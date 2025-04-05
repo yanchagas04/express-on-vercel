af79c37fb061961618beaf2fc783c4a930ffe3ed
yanchagasPC <yanchagas2004@gmail.com>
2025-04-05 00:18:11 -0300

    feat: Criando rota /tasks para gerar task aleatórias

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node' //IMPORTS PADRÃO PARA TER REQUEST/RESPONSE
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
---
+++ api/taks.ts
@@ -0,0 +1,20 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node' //IMPORTS PADRÃO PARA TER REQUEST/RESPONSE
+
+function generateId() {
+    return Math.floor(Math.random() * 10000)
+}
+
+function generateRandomDate() {
+    return Date.now() + Math.floor(Math.random() * 10000)
+}
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    return res.json({
+        id: generateId(),
+        title: 'Título da Tarefa',
+        description: 'Descrição da Tarefa',
+        completed: false,
+        createdAt: Date.now().toString(),
+        scheduledTo: generateRandomDate().toString(),
+    })
+}
