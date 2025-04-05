1c70bd0665af782705b6286e68a5f1241e064364
yanchagasPC <yanchagas2004@gmail.com>
2025-04-05 00:26:02 -0300

    fix: Coloca o Date como String

--- api/tasks.ts
+++ api/tasks.ts
@@ -5,16 +5,21 @@ function generateId() {
 }
 
 function generateRandomDate() {
-    return Date.now() + Math.floor(Math.random() * 10000)
+    return new Date(Date.now() + Math.floor(Math.random() * 10000)).toISOString()
+}
+
+function generateRandomBoolean() {
+    return Math.random() > 0.5
 }
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+    const id = generateId();
     return res.json({
-        id: generateId(),
-        title: 'Título da Tarefa',
-        description: 'Descrição da Tarefa',
-        completed: false,
-        createdAt: Date.now(),
+        id: id,
+        title: 'Título da Tarefa ' + id,
+        description: 'Descrição da Tarefa ' + id,
+        completed: generateRandomBoolean(),
+        createdAt: new Date(Date.now()).toISOString(),
         scheduledTo: generateRandomDate(),
     })
 }
